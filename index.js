b9b3ef0e4531af415220ee53ab4360bc44e42ea6
Ethan John <ethanjohnlagera@gmail.com>
2024-08-12 08:31:44 +0800

    Update index.js
    V1.1 Hour 1

--- index.js
+++ index.js
@@ -15,18 +15,21 @@ setDocDimensions(width, height);
 let rainColor = "gray" // I like either gray or blue
 let rainMaxLength = 15 // Adjust max length of the raindrops
 let rainRand = false // "false" for manual (rainIntensity) / "true" for random with rainIntensity as max
-let rainIntensity = 10 // 1:1 Raindrop, Higher intensity has more chance to overlap, I suggest < 6
+let rainIntensity = 2 // 1:1 Raindrop, Higher intensity has more chance to overlap, I suggest < 6
 
 // Cloud Creation
-let cloud = [1, 2, 3, 4];
-cloud[1] = new bt.Turtle();
-cloud[2] = new bt.Turtle();
-cloud[3] = new bt.Turtle();
-cloud[4] = new bt.Turtle();
+let cloud = [
+  0, // Filler
+  new bt.Turtle(), // 1
+  new bt.Turtle(), // 2
+  new bt.Turtle(), // 3
+  new bt.Turtle(), // 4
+  new bt.Turtle(), // 5 
+];
 
 // cloudOne
-let cloudOneBaseX = bt.randIntInRange(10, 20)
-const cloudOneBase = [cloudOneBaseX, 88]; // Static
+let cloudOneBaseX = bt.randIntInRange(10, 60)
+const cloudOneBase = [cloudOneBaseX, 80];
 cloud[1].up();
 cloud[1].goTo(cloudOneBase);
 cloud[1].down();
@@ -46,28 +49,24 @@ cloud[1].arc(106, 7);
 const cloudOneLines = cloud[1].lines();
 
 // cloudTwo
-let cloudTwoBaseX = bt.randIntInRange(20, 40)
-const cloudTwoBase = [cloudTwoBaseX, 97];
+let cloudTwoBaseX = bt.randIntInRange(10, 60)
+const cloudTwoBase = [cloudTwoBaseX, 90];
 cloud[2].up();
 cloud[2].goTo(cloudTwoBase);
 cloud[2].down();
-cloud[2].forward(52.9);
-cloud[2].arc(103, 8);
-cloud[2].setAngle(103);
-cloud[2].arc(86, 8);
-cloud[2].setAngle(-90);
-cloud[2].arc(146, -9);
-cloud[2].setAngle(155);
-cloud[2].arc(48, 12);
-cloud[2].setAngle(209);
-cloud[2].arc(72, 9);
-cloud[2].setAngle(142);
-cloud[2].arc(133.5, 13);
+cloud[2].forward(53.7);
+cloud[2].arc(190, 8);
+cloud[2].setAngle(80);
+cloud[2].arc(145, 11);
+cloud[2].setAngle(129);
+cloud[2].arc(138, 11);
+cloud[2].setAngle(149);
+cloud[2].arc(169, 12);
 const cloudTwoLines = cloud[2].lines();
 
 // cloudThree
-let cloudThreeBaseX = bt.randIntInRange(40, 60)
-const cloudThreeBase = [cloudThreeBaseX, 90];
+let cloudThreeBaseX = bt.randIntInRange(20, 60)
+const cloudThreeBase = [cloudThreeBaseX, 96];
 cloud[3].up();
 cloud[3].goTo(cloudThreeBase);
 cloud[3].down();
@@ -85,20 +84,71 @@ cloud[3].setAngle(142);
 cloud[3].arc(133.5, 13);
 const cloudThreeLines = cloud[3].lines();
 
-// bt.cover(cloudThreeLines, cloudTwoLines);
-// bt.cover(cloudThreeLines, cloudOneLines);
-// bt.cover(cloudTwoLines, cloudOneLines);
+// cloudFour
+let cloudFourBaseX = bt.randIntInRange(20, 60)
+const cloudFourBase = [cloudFourBaseX, 95];
+cloud[4].up();
+cloud[4].goTo(cloudThreeBase);
+cloud[4].down();
+cloud[4].forward(49.2);
+cloud[4].arc(180, 6);
+cloud[4].setAngle(90);
+cloud[4].arc(140, 9);
+cloud[4].setAngle(106);
+cloud[4].arc(140, 10);
+cloud[4].setAngle(135);
+cloud[4].arc(126, 10);
+cloud[4].setAngle(200);
+cloud[4].arc(160, 7);
+const cloudFourLines = cloud[4].lines();
 
-// drawLines(cloudOneLines, { stroke: "gray", width: 4 })
-drawLines(cloudTwoLines, { stroke: "gray", width: 4 })
-// drawLines(cloudThreeLines, { stroke: "gray", width: 4 })
+// cloudFive
+let cloudFiveBaseX = bt.randIntInRange(20, 60)
+const cloudFiveBase = [cloudFiveBaseX, 93];
+cloud[5].up();
+cloud[5].goTo(cloudThreeBase);
+cloud[5].down();
+cloud[5].forward(44.1);
+cloud[5].arc(233, 9);
+cloud[5].setAngle(83);
+cloud[5].arc(180, 10);
+cloud[5].setAngle(101);
+cloud[5].arc(179, 8);
+cloud[5].setAngle(174);
+cloud[5].arc(181, 7);
+const cloudFiveLines = cloud[5].lines();
 
+// Draw Clouds
+let cloudRandom = bt.randIntInRange(0, 3)
+if (cloudRandom == 0) {
+  bt.cover(cloudTwoLines, cloudOneLines);
+  drawLines(cloudOneLines, { stroke: "gray", width: 4 })
+  drawLines(cloudTwoLines, { stroke: "gray", width: 4 })
+} else if (cloudRandom == 1) {
+  bt.cover(cloudThreeLines, cloudTwoLines);
+  bt.cover(cloudThreeLines, cloudOneLines);
+  bt.cover(cloudTwoLines, cloudOneLines);
+  drawLines(cloudOneLines, { stroke: "gray", width: 4 })
+  drawLines(cloudTwoLines, { stroke: "gray", width: 4 })
+  drawLines(cloudThreeLines, { stroke: "gray", width: 4 })
+} else if (cloudRandom == 2) {
+  bt.cover(cloudFourLines, cloudTwoLines);
+  bt.cover(cloudFourLines, cloudOneLines);
+  bt.cover(cloudTwoLines, cloudOneLines);
+  drawLines(cloudOneLines, { stroke: "gray", width: 4 })
+  drawLines(cloudTwoLines, { stroke: "gray", width: 4 })
+  drawLines(cloudFourLines, { stroke: "gray", width: 4 })
+} else if (cloudRandom == 3) {
+  bt.cover(cloudFiveLines, cloudTwoLines);
+  bt.cover(cloudFiveLines, cloudOneLines);
+  bt.cover(cloudTwoLines, cloudOneLines);
+  drawLines(cloudOneLines, { stroke: "gray", width: 4 })
+  drawLines(cloudTwoLines, { stroke: "gray", width: 4 })
+  drawLines(cloudFiveLines, { stroke: "gray", width: 4 })
+}
 
 // Rain Generation
-
-// Pick rain amount
-let rainFinalSet = 0
-
+let rainFinalSet; // Rain Amount
 if (rainRand == false) {
   rainFinalSet = rainIntensity
 } else if (rainRand == true) {
@@ -108,6 +158,9 @@ if (rainRand == false) {
 // Main Generation
 let finalLines = [];
 let cutLines = [];
+const raindrops = [];
+let rainOne = [];
+let rainTwo = [];
 
 for (let i = 0; i < rainFinalSet; i++) {
   // Raindrop shape variables
@@ -118,7 +171,7 @@ for (let i = 0; i < rainFinalSet; i++) {
   let rightSide = leftSide + bt.randInRange(3, maxWidth);
   let sidesDifference = (rightSide - leftSide) / 2;
   let center = sidesDifference + leftSide;
-
+  let covered;
 
   //Raindrop Shaping
   const raindrop = bt.catmullRom([
@@ -126,10 +179,20 @@ for (let i = 0; i < rainFinalSet; i++) {
     [leftSide, base],
     [rightSide, base],
     [center, tip]
-  ])
+  ]);
 
-  finalLines.push(raindrop);
-  bt.cover(cutLines, finalLines);
-  drawLines(finalLines, { stroke: rainColor, width: 2 });
-  cutLines.push(finalLines);
+  // raindrops.push(raindrop);
+  // console.log(raindrop)
+  //console.log(raindrops[0])
+  if (i > 0 ) {
+    rainTwo.push(raindrop);
+    bt.cover(rainOne, rainTwo);
+  } else if (i == 0) {
+    rainOne.push(raindrop);
+  }
+  
+  finalLines.push(raindrops[i]);
 }
+
+finalLines.push(raindrops);
+drawLines(finalLines, { stroke: rainColor, width: 2 });
